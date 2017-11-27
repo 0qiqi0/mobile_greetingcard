057b1a8fc1e6174a0d06d772a48a1dc35ba50d8c
qiqi <1483593015@qq.com>
2017-11-27 21:27:16 +0800

    all is ok

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 /**
  * Created by qiqi on 17/11/25.
  */
+var page1 = document.getElementById("page1");
+var page2 = document.getElementById("page2");
+var page3 = document.getElementById("page3");
 
 var music = document.getElementById('music_c');
 var audio = document.getElementsByTagName('audio')[0];
@@ -44,3 +47,17 @@ music.addEventListener('touchstart',function(event){
         this.style.animationPlayState = 'paused';
     }
 },false);
+
+//点击屏幕开启好运
+page1.addEventListener('touchstart',function(event){
+    page1.style.display = 'none';
+    page2.style.display = 'block';
+    page3.style.display = 'block';
+    page3.style.top     = '100%';
+
+    setTimeout(function(){
+        page2.setAttribute('class','page fadeOut');
+        page3.setAttribute('class','page fadeIn')
+
+    },5500)
+},false)
