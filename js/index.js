695457359c472293274f38468f28eb87fb2ba8fc
qiqi <1483593015@qq.com>
2017-12-19 17:23:46 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -8,19 +8,23 @@ var page3 = document.getElementById("page3");
 var music = document.getElementById('music_c');
 var audio = document.getElementsByTagName('audio')[0];
 
+//vh,vw自适应问题.
+//第二页过渡完后全白解决:
+
 audio.addEventListener('ended',function(event){
-    music.setAttribute('class','');
+    music.setAttribute('class',''); //让唱片磁头移动开
+
     //this.setAttribute('class','play');
     //this.style.webkitAnimationPlayState ='paused';
 
 },false);
-/*
 
+/*//音乐播放暂停控制
 music.onclick = function(){
     if(audio.paused){
         audio.play();
         //this.setAttribute('class','play'); //安卓4.0以下不行
-        this.style.animationPlayState = 'running'
+        this.style.animationPlayState = 'running';
         //this.style.webkitAnimationPlayState = 'running' //适用于苹果6p以下
 
     }else{
@@ -29,20 +33,20 @@ music.onclick = function(){
         this.style.animationPlayState = 'paused';
         //this.style.webkitAnimationPlayState ='paused';
     }
-}
-*/
+}*/
+
 
 //手机的单击事件有300ms的延迟来判断是否是长按.
 music.addEventListener('touchstart',function(event){
     if(audio.paused){
         audio.play();
-        //this.setAttribute('class','play'); //安卓4.0以下不行
-        //this.style.webkitAnimationPlayState = 'running' //适用于苹果6p以下
+        this.setAttribute('class','play'); //播放唱片旋转动画,安卓4.0以下不行
+        //this.style.webkitAnimationPlayState = 'running'; //适用于苹果6p以下
         this.style.animationPlayState = 'running'
 
     }else{
         audio.pause();
-        //this.setAttribute('class','');
+        this.setAttribute('class','');
         //this.style.webkitAnimationPlayState ='paused';
         this.style.animationPlayState = 'paused';
     }
@@ -51,13 +55,17 @@ music.addEventListener('touchstart',function(event){
 //点击屏幕开启好运
 page1.addEventListener('touchstart',function(event){
     page1.style.display = 'none';
+
     page2.style.display = 'block';
     page3.style.display = 'block';
     page3.style.top     = '100%';
 
-    setTimeout(function(){
+/*    setTimeout(function(){
         page2.setAttribute('class','page fadeOut');
         page3.setAttribute('class','page fadeIn')
+    },5500);*/
 
-    },5500)
-},false)
+},false);
+
+
+
